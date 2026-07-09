2e85ec94695f89579410dbcf08871a958c0d6420
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2026-07-09 10:51:58 +0300

    feat: sign-in/up pages + protected (app) route group with bottom-tab shell

---
+++ app/(app)/more/page.tsx
@@ -0,0 +1,3 @@
+export default function MorePage() {
+  return <h1 className="text-xl font-semibold">More</h1>
+}
---
+++ app/(app)/plan/page.tsx
@@ -0,0 +1,3 @@
+export default function PlanPage() {
+  return <h1 className="text-xl font-semibold">Plan</h1>
+}
